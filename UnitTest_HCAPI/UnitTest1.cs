20fb38f3d3e317dfedb9cc9b47ed292112a21830
Péter Szalay <peter.szalay@stud.uni-corvinus.hu>
2023-05-01 15:19:05 +0200

    Initial commit

---
+++ UnitTest_HCAPI/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest_HCAPI
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
